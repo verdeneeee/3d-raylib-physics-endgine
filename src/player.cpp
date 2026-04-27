b156f89c13271d6517a8e7873d82ebc7b68cf2f0
verden <155012457+verdeneeee@users.noreply.github.com>
2026-04-27 17:41:25 +0300

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,7 +1,38 @@
 #include "player.h"
 #include "world.h"
 
-Player::Player(Vector3 startPos) : velocity{ 0, 0, 0 }, isGrounded(true), maxSpeed(0.5f) {
+Vector3 Vector3Subtract(Vector3 vec1, Vector3 vec2) 
+{
+    return { vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z };
+}
+
+Vector3 Vector3Add(Vector3 vec1, Vector3 vec2)
+{
+    return { vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z };
+}
+
+Vector3 Vector3Scale(Vector3 vec, float scale) 
+{
+    return { vec.x * scale, vec.y * scale, vec.z * scale };
+}
+
+float Vector3Length(Vector3 vec)
+{
+    return sqrtf(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+Vector3 Vector3Normalize(Vector3 vec)
+{
+    float length = Vector3Length(vec);
+    if (length > 0)
+    {
+        return { vec.x / length, vec.y / length, vec.z / length };
+    }
+    return { 0, 0, 0 };
+}
+
+Player::Player(Vector3 startPos) : velocity{ 0, 0, 0 }, isGrounded(true), maxSpeed(0.5f)
+{
 	cam.fovy = 90;
 	cam.position = startPos;
 	cam.target = { 0, 1, 0 };
@@ -31,34 +62,67 @@ void Player::jump(const World& world, float deltaTime)
 
 void Player::movemant(World& world, float deltaTime)
 {
-	UpdateCameraPro(&cam,
-		{ velocity.x, velocity.z, 0.0f },
-		{ GetMouseDelta().x * 0.13f, GetMouseDelta().y * 0.13f }, 0);
+    UpdateCameraPro(&cam,
+        { velocity.x, velocity.z, 0.0f },
+        { GetMouseDelta().x * 0.13f, GetMouseDelta().y * 0.13f }, 0);
 
-	cam.target.y += velocity.y * deltaTime;
+    cam.target.y += velocity.y * deltaTime;
 
-	if (IsKeyDown(KEY_W)) velocity.x += world.accelerate * deltaTime;
-	if (IsKeyDown(KEY_S)) velocity.x -= world.accelerate * deltaTime;
-	if (IsKeyDown(KEY_D)) velocity.z += world.accelerate * deltaTime;
-	if (IsKeyDown(KEY_A)) velocity.z -= world.accelerate * deltaTime;
+    if (IsKeyDown(KEY_W)) velocity.x += world.accelerate * deltaTime;
+    if (IsKeyDown(KEY_S)) velocity.x -= world.accelerate * deltaTime;
+    if (IsKeyDown(KEY_D)) velocity.z += world.accelerate * deltaTime;
+    if (IsKeyDown(KEY_A)) velocity.z -= world.accelerate * deltaTime;
 
-	velocity.x *= world.friction;
-	velocity.z *= world.friction;
+    velocity.x *= world.friction;
+    velocity.z *= world.friction;
 
-	if (velocity.x > maxSpeed) velocity.x = maxSpeed;
-	if (velocity.x < -maxSpeed) velocity.x = -maxSpeed;
-	if (velocity.z > maxSpeed) velocity.z = maxSpeed;
-	if (velocity.z < -maxSpeed) velocity.z = -maxSpeed;
+    if (velocity.x > maxSpeed) velocity.x = maxSpeed;
+    if (velocity.x < -maxSpeed) velocity.x = -maxSpeed;
+    if (velocity.z > maxSpeed) velocity.z = maxSpeed;
+    if (velocity.z < -maxSpeed) velocity.z = -maxSpeed;
 
-	cam.position.x += velocity.x * deltaTime;
-	cam.position.z += velocity.z * deltaTime;
+    Vector3 forward = Vector3Subtract(cam.target, cam.position);
 
-	float playerRadius = 0.3f;
+    cam.position.x += velocity.x * deltaTime;
+    cam.position.z += velocity.z * deltaTime;
 
-	if (cam.position.x + playerRadius > world.wallDist) cam.position.x = world.wallDist - playerRadius;
-	if (cam.position.x - playerRadius < -world.wallDist) cam.position.x = -world.wallDist + playerRadius;
-	if (cam.position.z + playerRadius > world.wallDist) cam.position.z = world.wallDist - playerRadius;
-	if (cam.position.z - playerRadius < -world.wallDist) cam.position.z = -world.wallDist + playerRadius;
+    float playerRadius = 0.3f;
+    bool collided = false;
+
+    if (cam.position.x + playerRadius > world.wallDist)
+    {
+        cam.position.x = world.wallDist - playerRadius;
+        collided = true;
+    }
+
+    if (cam.position.x - playerRadius < -world.wallDist) 
+    {
+        cam.position.x = -world.wallDist + playerRadius;
+        collided = true;
+    }
+
+    if (cam.position.z + playerRadius > world.wallDist) 
+    {
+        cam.position.z = world.wallDist - playerRadius;
+        collided = true;
+    }
+
+    if (cam.position.z - playerRadius < -world.wallDist) 
+    {
+        cam.position.z = -world.wallDist + playerRadius;
+        collided = true;
+    }
+
+    if (collided) 
+    {
+        float length = Vector3Length(forward);
+        if (length > 0) 
+        {
+            forward = Vector3Normalize(forward);
+            forward = Vector3Scale(forward, length);
+            cam.target = Vector3Add(cam.position, forward);
+        }
+    }
 }
 
 void Player::update(World& world, float deltaTime)
